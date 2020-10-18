bbfb517e529b00370f44d4a2b6cac29c1d378c0d
LegNBass <charmentroutv@gmail.com>
2020-10-18 15:17:33 -0400

    Added randomization and validation

--- ssanta.py
+++ ssanta.py
@@ -5,14 +5,51 @@ for each person. People in the same inner list cannot be matched.
 import json
 import random
 
-def randomize(couples: list):
+from functools import reduce
+
+
+def flatten(_list):
+    """
+    Takes a list of lists and returns a songle list containing all items
+
+    :_list list: The list to flatten
+
+    :return list: The flat list
+    """
+    return reduce(
+        lambda x, y: y + x,
+        _list,
+        []
+    )
+
+
+def valid(exclusions, flat_list):
+    """
+    Determines if the flat list meets the couples requirements
+
+    :exclusions list: The list of lists used to invalidate the shuffle
+    :flast_list list: The current shuffle to validate
+
+    :return bool: Is or is not a valid shuffle
+    """
+    flag = False
+    for ix, name in enumerate(flat_list):
+        pair = {name, flat_list[(ix + 1) % len(flat_list)]}
+        for group in exclusions:
+            flag |= pair <= set(group)
+    return not flag  # result
+
+
+def randomize(exclusions: list):
     """
     Does the randomizing
     """
-    random.shuffle(
-        couples
-    )
-    return couples
+    flat_list = flatten(exclusions)
+    while not valid(exclusions, flat_list):
+        random.shuffle(
+            flat_list
+        )
+    return flat_list
 
 
 if __name__ == '__main__':
@@ -26,10 +63,10 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    print(args.list_file)
-
     with open(args.list_file) as _f:
         couples = json.load(_f)
+
+        # Print the result of the randomization to the shell
         print(
             randomize(couples)
         )
